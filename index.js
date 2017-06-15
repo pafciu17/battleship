76051f6f948bd906225f6561aad6106cb1687af5
Olli Karppinen <olli.karppinen@relex.fi>
2017-06-15 15:35:26 +0300

    Adds body parser

--- index.js
+++ index.js
@@ -1,18 +1,34 @@
 var express = require('express')
 var app = express()
 
+var bodyParser = require( './node_modules/body-parser' );
+
+app.use(bodyParser.json());
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/start_game', function (req, res) {
     res.send('hello world')
 })
 
-app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
+app.post('/next_turn', (req, res) => {
+  res.send(`POST /next_turn. JSON: ${JSON.stringify(req.body)}`)
 })
+
+app.post('/end_game', (req, res) => {
+  res.send(`POST /end_game. JSON: ${JSON.stringify(req.body)}`)
+})
+
 
 MISS = 'o'
 HIT = 'x'
 SUNK = 's'
 
-const enemyBoard = Array(10).fill(Array(10))
-const ourBoard = Array(10).fill(Array(10))
+const enemyBoard = new Array(10).fill(new Array(10))
+const ourBoard = new Array(10).fill(new Array(10))
+
+const updateOurBoard = (x, y) => {
+  
+}
